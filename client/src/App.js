1cca523905d8ed338ff5fdc655deae1231be59a3
Alyzen Jeraj <38901318+alyzenjeraj@users.noreply.github.com>
2022-08-22 19:42:39 -0600

    Added Update feature

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { getPosts } from './actions/posts'
+import memories from './images/memories.png'
+import Posts from './components/Posts'
+import Form from './components/Form'
+
+const App = () => {
+    const [currentId, setCurrentId] = useState(null)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch, currentId])
+
+    return (
+        <Container maxwidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Memories</Typography>
+                <img src={memories} alt='memories' height='60'/>
+                
+            </AppBar>
+            <Grow in>
+                <Container >
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts setCurrentId={setCurrentId}/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
+}
+
+export default App
